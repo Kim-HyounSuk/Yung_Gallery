d0c69412cc40c17125cf2a89ff3be4b28e0563fb
Kim-HyounSuk <kkoor586@naver.com>
2025-10-28 12:00:41 +0900

    🚚 Move: 폴더 구조 아키텍쳐 변경

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SaudiForm } from '@/widgets/saudi/ui'
-
-export default function Home() {
-  return (
-    <div className="flex min-h-svh flex-col md:min-h-screen">
-      <SaudiForm />
-    </div>
-  )
-}
