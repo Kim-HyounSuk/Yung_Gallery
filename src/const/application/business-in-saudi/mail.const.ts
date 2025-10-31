90ffcd55b4bdbad3badf67fddd04c3eec64b6ae7
Kim-HyounSuk <kkoor586@naver.com>
2025-10-31 20:17:15 +0900

    🩹 Remove: 테스트용 더미 데이터 제거

--- src/const/application/business-in-saudi/mail.const.ts
+++ src/const/application/business-in-saudi/mail.const.ts
@@ -1,20 +1,18 @@
 import { Manager, ManagerMap } from '@/type/application'
 
 export const MANAGER_MAP: ManagerMap = {
-  partnership: [{ email: 'vanlan6499@gmail.com', name: '파트너십 담당자' }],
-  license: [{ email: 'vanlan6499@gmail.com', name: '라이센스 담당자' }],
-  export: [{ email: 'vanlan6499@gmail.com', name: '해외유통 담당자' }],
-  incubator: [{ email: 'vanlan6499@gmail.com', name: '인큐베이터 담당자' }],
-  dammam_mall: [{ email: 'vanlan6499@gmail.com', name: '담암몰 담당자' }],
-  business_service: [
-    { email: 'vanlan6499@gmail.com', name: '비즈니스서비스 담당자' },
-  ],
-  other: [{ email: 'vanlan6499@gmail.com', name: '기타문의 담당자' }],
+  partnership: [],
+  license: [],
+  export: [],
+  incubator: [],
+  dammam_mall: [],
+  business_service: [],
+  other: [],
 }
 
 // 최고 담당자 (모든 관심분야의 메일을 받음)
 export const SUPER_MANAGER: Manager = {
-  email: 'vanlan6499@gmail.com',
+  email: process.env.SUPER_MANAGER!,
   name: '최고 담당자',
   isSuper: true,
 }
